071f86a6ad5d649ce90d10fe53ec16971c2833ce
himanshu <himanshurojwal321@gmail.com>
2025-05-19 23:54:55 +0530

    values and variables in js

---
+++ file21/script.js
@@ -0,0 +1,23 @@
+let js = "amazing";
+console.log(48+45-32);
+
+console.log('jones');
+console.log(23);
+
+let firstName = "himanshu";
+console.log(firstName);
+
+let $function = "jones";
+let PI = 3.1415;
+
+let myFirstJob = "Programmer";
+let myCurrentJob = "Teacher";
+console.log(myCurrentJob);
+
+let country = 'Portugal';
+let continent = 'Europe';
+let population = 10;
+
+console.log(country);
+console.log(continent);
+console.log(population);
