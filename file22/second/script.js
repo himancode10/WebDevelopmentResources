3dc522f5c2aeb48e249fa7a25cca3b5832b0e08e
himanshu <himanshurojwal321@gmail.com>
2025-05-20 21:50:22 +0530

    file22

---
+++ file22/second/script.js
@@ -0,0 +1,31 @@
+
+
+// DATA 1
+
+let markMass = 78 ;//kg
+let markHeight = 1.69  ; //m
+let johnMass = 92 ; //kg
+let johnHeight= 1.95  ; //m
+
+let markBMI1 = markMass / (markHeight * markHeight);
+let johnBMI1 = johnMass / (johnHeight * johnHeight);
+let markHigherBMI1 = markBMI1 > johnBMI1;
+console.log(markBMI1);
+console.log(johnBMI1);
+console.log(markHigherBMI1);
+
+// DATA 2
+
+let markMass1 = 95 ;//kg
+let markHeight1 = 1.88  ; //m
+let johnMass1 = 85 ; //kg
+let johnHeight1= 1.76  ; //m
+
+let markBMI2 = markMass1/(markHeight1*markHeight1);
+let johnBMI2 = johnMass1/(johnHeight1*johnHeight1);
+let markHigherBMI2 = markBMI2 > johnBMI2;
+console.log(markBMI2);
+console.log(johnBMI2);
+console.log(markHigherBMI2);
+
+ 
