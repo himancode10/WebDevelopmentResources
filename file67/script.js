bcaa4d25c681b7d26ac99458111defc50dd891bf
himanshu <himanshurojwal321@gmail.com>
2025-07-09 00:53:06 +0530

    findLast and findLastIndex method

---
+++ file67/script.js
@@ -0,0 +1,26 @@
+'use strict';
+
+
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+
+// The New findLast and findLastIndex Methods
+
+console.log(movements);
+const lastWithdrawal = movements.findLast(mov => mov < 0);
+console.log(lastWithdrawal);
+
+// 'Your latest large movement was X movements ago'
+
+const latestLargeMovementIndex = movements.findLastIndex(
+  mov => Math.abs(mov) > 2000
+);
+console.log(latestLargeMovementIndex);
+console.log(
+  `Your latest large movement was ${
+    movements.length - latestLargeMovementIndex
+  } movements ago`
+);
+
+
+
+
